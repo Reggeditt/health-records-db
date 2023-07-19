34b79d6a859923e35d0991d25311df3e0428ff5f
Reggeditt <tkodjosarso@gmail.com>
2023-07-19 09:17:46 +0000

    add join table to handle many-to-many relationship

--- schema_based_on_diagram.sql
+++ schema_based_on_diagram.sql
@@ -13,11 +13,19 @@ CREATE TABLE medical_histories (
   status VARCHAR(50)
 );
 
+-- add join table between medical histories and treatments
+CREATE TABLE medical_chart (
+  medical_history_id INTEGER NOT NULL REFERENCES medical_histories(id),
+  treatment_id INTEGER NOT NULL REFERENCES treatments(id),
+);
+
+
 CREATE TABLE treatments(
   id SERIAL PRIMARY KEY,
   type VARCHAR(255) NOT NULL,
   name VARCHAR(255)
 );
+
 CREATE TABLE invoices(
   id SERIAL PRIMARY KEY,
   total_amount DECIMAL,
@@ -25,6 +33,7 @@ CREATE TABLE invoices(
   paid_at_timestamp DEFAULT CURRENT_TIMESTAMP,
   medical_history_id INTEGER REFERENCES medical_histories(id)
 );
+
 CREATE TABLE invoice_items(
   id SERIAL PRIMARY KEY,
   unit_price DECIMAL,
